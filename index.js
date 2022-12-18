4811453caeab5d88ba7603d7daa89cfa8e78aabd
Neel Thakkar <57473461+Neel278@users.noreply.github.com>
2022-12-18 11:59:59 +0530

    Image upload path added

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const fileUpload = require("express-fileupload");
+const awsService = require("./awsService");
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world this is a node app by Neel!");
+app.use(fileUpload());
+// app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", async (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post("/upload", async (req, res) => {
+  const { image } = req.files;
+  try {
+    await awsService.uploadImage(image.name, image.data);
+  } catch (e) {
+    console.log(e);
+  }
+  res.redirect("/");
 });
 
 const PORT = process.env.PORT || 3000;
